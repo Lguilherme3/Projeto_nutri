5efd596e318d743666cdddd0ac40e7c7fe602835
Guilherme <gui_3@outlook.com>
2022-10-30 17:52:06 -0300

    aula final, ReGex + Ajax

--- js/form.js
+++ js/form.js
@@ -9,7 +9,7 @@ botaoAdicionar.addEventListener("click", function(event){
     var paciente = obtemPacienteDoFormulario(form);
 
     // criar a tr a td do paciente
-    var pacienteTr = montaTr(paciente);
+    
 
     var erros = validaPaciente(paciente);
         console.log(erros);
@@ -17,16 +17,20 @@ botaoAdicionar.addEventListener("click", function(event){
             exibeMensagensDeErro(erros);
             return;
         }
-
-
-    var tabela = document.querySelector("#tabela-pacientes")
-
-    tabela.appendChild(pacienteTr);
+    
+    adicionaPacienteNaTabela(paciente);
+    
 
     form.reset();
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = ""
 });
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes")
+    tabela.appendChild(pacienteTr);
+}
 
 function exibeMensagensDeErro(erros){
     var ul = document.querySelector("#mensagens-erro");
